902110a8aa6b36af3c9f53e8a1a1a807943ed5d1
malickalex1975 <malickalex1975@gmail.com>
2022-04-27 00:45:39 +0300

    update

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(){
+    this.stk=[];
+  }
+  push( elem) {
+    this.stk.push(elem);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stk.pop()
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+return this.stk[this.stk.length-1]
   }
 }
 
